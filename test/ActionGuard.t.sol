c0a74c531891365be64694b5b83a4108129c9cf9
Kelpejol <141008634+Kelpejol@users.noreply.github.com>
2025-12-26 07:10:20 -1200

    Update ActionGuard.t.sol

--- test/ActionGuard.t.sol
+++ test/ActionGuard.t.sol
@@ -7,6 +7,7 @@ import "../src/ActionGuard.sol";
 contract ActionGuardTest is Test {
     ActionGuard guard;
     address user = address(1);
+    address admin = address(this);
 
     function setUp() public {
         guard = new ActionGuard(100 ether, 1 days);
@@ -31,5 +32,48 @@ contract ActionGuardTest is Test {
         vm.prank(user);
         vm.expectRevert(ActionGuard.CooldownNotPassed.selector);
         guard.check(user, 10 ether);
+    }
+
+    function testCooldownPassesAfterTime() public {
+        vm.prank(user);
+        guard.recordExecution(user);
+
+        // Fast forward past cooldown
+        vm.warp(block.timestamp + 1 days + 1);
+
+        vm.prank(user);
+        assertTrue(guard.check(user, 10 ether));
+    }
+
+    function testUnauthorizedUserRejected() public {
+        address unauthorized = address(2);
+        
+        vm.prank(unauthorized);
+        vm.expectRevert(ActionGuard.NotAuthorized.selector);
+        guard.check(unauthorized, 10 ether);
+    }
+
+    function testAdminCanUpdatePolicy() public {
+        guard.updatePolicy(200 ether, 2 days);
+        
+        assertEq(guard.maxAmount(), 200 ether);
+        assertEq(guard.cooldown(), 2 days);
+    }
+
+    function testNonAdminCannotUpdatePolicy() public {
+        vm.prank(user);
+        vm.expectRevert();
+        guard.updatePolicy(200 ether, 2 days);
+    }
+
+    function testRecordExecutionUpdatesTimestamp() public {
+        uint256 beforeTimestamp = guard.lastExecution(user);
+        
+        vm.prank(user);
+        guard.recordExecution(user);
+        
+        uint256 afterTimestamp = guard.lastExecution(user);
+        assertTrue(afterTimestamp > beforeTimestamp);
+        assertEq(afterTimestamp, block.timestamp);
     }
 }
